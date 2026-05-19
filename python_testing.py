f172e296b1b2aea6c417a3eff4630614a26e396e
IdaEinarsen <ida.einarsen@edunord.se>
2026-05-19 10:49:57 +0200

    I tried doing loops in lists

--- python_testing.py
+++ python_testing.py
@@ -1,1 +1,15 @@
 print('This is where i test things in python')
+
+# How to display each name as its own. + with a bigletter using .title()
+magicians = ['houdini','mr.amazing','terrifico']
+for magician in magicians:
+    print(magician.title())
+
+list_of_items = ['baseball','pen','shoe','car','bottle']
+for item in list_of_items:
+    print(f"{item.title()} is green")
+    print(f"I would want the {item} to be red")
+print("I guess green will have to work! ")
+
+
+   
