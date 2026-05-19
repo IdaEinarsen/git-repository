e2729b8829024c071e7db6de8c0ca0d08d672289
IdaEinarsen <ida.einarsen@edunord.se>
2026-05-19 12:11:09 +0200

    Keept working with chapter 4

--- python_testing.py
+++ python_testing.py
@@ -11,5 +11,38 @@ for item in list_of_items:
     print(f"I would want the {item} to be red")
 print("I guess green will have to work! ")
 
+# Using the range() function 
+
+for value in range (1, 5):
+    print(value)
+
+# How to use range function to get even numbers
+
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
+
+
+numbers = list(range(1, 12, 1))
+print(numbers)
+
+# Square with range
+
+squares = []
+for value in range(1, 11):
+    square = value ** 2
+    squares.append(square)
+print(squares)
+
+
+# simple statistics with digits
+
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print(min(digits))
+print(max(digits))
+print(sum(digits))
+
+
+
+
 
    
