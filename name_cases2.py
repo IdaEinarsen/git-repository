703352466dec3aaed023e23f1c4d5b7c9310321a
IdaEinarsen <ida.einarsen@edunord.se>
2026-05-20 13:20:47 +0200

    Delete name_cases2.py

--- name_cases2.py
+++
@@ -1,5 +0,0 @@
-# 2.5 Famous quote 
-
-quote = '"Be who you are and say what you feel, because those who mind dont matter, and those who matter dont mind"'
-name = "Bernard M. Baruch"
-print(f"{name} once said, {quote}")
