4c3084a3772e81a3a3e6a292698f78acc7ec6216
IdaEinarsen <ida.einarsen@edunord.se>
2026-05-20 12:06:49 +0200

    Exercise 2.1 - 2.2

---
+++ simple_message.py
@@ -0,0 +1,11 @@
+# 2.1 Simple message's
+message = "Hello python world!"
+print(message)
+
+# 2.2 Simple message's 
+message = "Hello everyone!"
+print (message)
+
+message = "I like food."
+print(message)
+
