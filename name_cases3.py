de9052da267122999ee964cf1e993c22f1f3adbc
IdaEinarsen <ida.einarsen@edunord.se>
2026-05-20 12:40:53 +0200

    Exercise 2.6

---
+++ name_cases3.py
@@ -0,0 +1,5 @@
+# 2.6 Famous quotes
+
+famous_person = 'Zig Zaglar'
+message = '"Until you are happy with who you are, you will never be happy with what you have."'
+print(f'{famous_person} once said, {message}')
