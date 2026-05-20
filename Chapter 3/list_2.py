e1ae6a938a4bc88bf4c5e67a1dd7c569c3c82b92
IdaEinarsen <ida.einarsen@edunord.se>
2026-05-20 14:12:57 +0200

    Added chapter 3 folder with 3 exercises done

---
+++ Chapter 3/list_2.py
@@ -0,0 +1,7 @@
+# 3.3 Your own list
+
+favorite_car = ['alfa romeo', 'volvo', 'audi', 'suzuki']
+print(f"I would like to own a {favorite_car[0].title()}")
+print(f"I think {favorite_car[1]} is a bad car.")
+print(f"{favorite_car[2].title()} is the best car.")
+print(f"I want to try a {favorite_car[3]}.")
