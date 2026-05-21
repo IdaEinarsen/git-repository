fec411f21283bb4203ed93d768d54067b8f4786f
Ida <ida.einarsen@edunord.se>
2026-05-21 11:54:08 +0200

    Exercises from chapter 4

---
+++ Chapter 4/more_lists.py
@@ -0,0 +1,12 @@
+# 4.1 Pizza
+type_pizza = ['hawaii', 'hakis', 'kebab', 'vesuvio']
+message = "Pizza is amazing!"
+
+for pizza in type_pizza:
+    print(f"{pizza.title()} is the best pizza!")
+
+print(message)
+
+
+# I picked 4 type of pizza's. The first one is my favorite and the other 3
+# is my friends favorite's.
---
+++ Chapter 4/more_lists1.py
@@ -0,0 +1,10 @@
+#4.2 Animals
+animals = ['dog', 'cat', 'bunny']
+message= "All these animal's have fur and are pet's"
+
+for animal in animals:
+    print(f"{animal.title()} is a great pet")
+print(message)
+
+# I picked these animal's because all of them are pets and have fur.
+
