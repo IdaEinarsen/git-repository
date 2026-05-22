17111f24f6aacfe7df740789632fb0fcc5631f61
Ida <ida.einarsen@edunord.se>
2026-05-22 11:55:33 +0200

    chapter 5 first exercise and fixed small things

---
+++ Chapter_5/conditional_test.py
@@ -0,0 +1,57 @@
+# 5.1 Conditional Tests
+
+# I followed the example and did 5 true and 5 false
+
+car = 'suzuki'
+
+print("Is car == 'subaru'? I predict True.")
+print(car == 'suzuki')
+
+print("\nIs car == 'honda'? I predict False.")
+print(car == 'honda')
+
+
+ship = 'titanic'
+
+print("\nIs animal == 'titanic'? I predict True.")
+print(ship == 'titanic')
+
+print("\nIs animal == 'olympic'? I predict False.")
+print(ship == 'olympic')
+
+
+number = 10
+
+print("\nIs number > 5? I predict True.")
+print(number > 5)
+
+print("\nIs number < 5? I predict False.")
+print(number < 5)
+
+
+color = 'yellow'
+
+print("\nIs color == 'yellow'? I predict True.")
+print(color == 'yellow')
+
+print("\nIs color == 'green'? I predict False.")
+print(color == 'green')
+
+
+food = 'sushi'
+
+print("\nIs food == 'chicken'? I predict True.")
+print(food != 'chicken')
+
+print("\nIs food == 'salad'? I predict False.")
+print(food == 'salad')
+
+
+age = 18
+
+print("\nIs age >= 18? I predict True.")
+print(age >= 18)
+
+print("\nIs age < 18? I predict False.")
+print(age < 18)
+
