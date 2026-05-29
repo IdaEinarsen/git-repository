360be3c142f09407d5d11a3ac1f8bd63cabcc21d
Ida <ida.einarsen@edunord.se>
2026-05-29 09:41:50 +0200

    chapter 9 + journal entry

--- Chapter_9/imported_restaurant.py
+++ Chapter_9/imported_restaurant.py
@@ -1,2 +1,14 @@
-# 9.10 Imported Restaurant
+# 9.10 Restaurant
+
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print("Restaurant Name:", self.restaurant_name)
+        print("Cuisine Type:", self.cuisine_type)
+
+    def open_restaurant(self):
+        print(self.restaurant_name + " is now open!")
 
---
+++ Chapter_9/main restaurant.py
@@ -0,0 +1,8 @@
+# 9.10.1 Imported Restaurant
+
+from imported_restaurant import Restaurant
+
+my_restaurant = Restaurant("Subway", "Italian BT")
+
+my_restaurant.describe_restaurant()
+
