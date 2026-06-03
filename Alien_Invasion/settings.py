6923f07e3b871f737fd04649c4be9e1f50ec2772
IdaEinarsen <ida.einarsen@edunord.se>
2026-06-03 12:54:43 +0200

    fixed movement for ship and speed settings

--- Alien_Invasion/settings.py
+++ Alien_Invasion/settings.py
@@ -3,7 +3,12 @@ class Settings:
 
     def __init__(self):
         """Initializa the game's settings."""
+        
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+
+
+        # Ship Settings
+        self.ship_speed = 4.5
