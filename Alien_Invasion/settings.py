26785b0e89067ece34ee802636e3f9ee7eac029d
IdaEinarsen <ida.einarsen@edunord.se>
2026-06-10 12:32:27 +0200

    Finished alien invasion

--- Alien_Invasion/settings.py
+++ Alien_Invasion/settings.py
@@ -17,7 +17,7 @@ class Settings:
 
         # Bullet Settings
         self.bullet_speed = 6.0
-        self.bullet_width = 900                         # 3 original
+        self.bullet_width = 5                        # 3 original
         self.bullet_height = 15                        # 15 orignial
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 5
