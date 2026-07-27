7a2af3f6c66ee12a95034b8ffe042d649e415834
Ida <ida.einarsen@edunord.se>
2026-07-27 14:35:06 +0200

    15.6

--- Chapter_15/15.6/die_visual.py
+++ Chapter_15/15.6/die_visual.py
@@ -1,11 +1,31 @@
+import plotly.express as px
+
 from die import Die
 
-#Create a D6.
-die = Die()
+#Cahnged the dices to D8
+die_1 = Die(8)
+die_2 = Die(8)
 
 # Make some rolls, and store results in a list.
 results = []
-for roll_num in range(100):
-  result = die.roll()
+for roll_num in range(100000):
+  result = die_1.roll() + die_2.roll()
   results.append(result)
-print(results)
+
+# Analyze the results.
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_result+1)
+for value in poss_results:
+  frequency = results.count(value)
+  frequencies.append(frequency)
+
+# Visualize the results.
+title = "Results of Rolling Two D8 Dice 1,000 Times"
+labels = {'x': 'Results', 'y': 'Frequency of Result'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+fig.show()
+
+print(frequencies)
+
+
